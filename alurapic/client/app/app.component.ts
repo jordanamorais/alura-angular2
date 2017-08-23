3bad6f5b48d7730ed342e3d4b27435ae647a0427
Jordana Morais <moraisjordana@gmail.com>
2017-08-23 20:03:41 -0300

    Removed node_modules folder, added some notes and initial http request

--- alurapic/client/app/app.component.ts
+++ alurapic/client/app/app.component.ts
@@ -1,7 +1,11 @@
-import { selector } from 'rxjs/operator/publish';
+
+
 // Decorator - Processado pelo TypeScript
 // ---
-import { Component } from '@angular/core';
+// Só com Typescript, usando a tipagem, nao precisa importar o Inject.
+import { Component } from '@angular/core'; // Component, Para criar um Componente, Injector para requisições
+import { selector } from 'rxjs/operator/publish'; 
+import { Http } from '@angular/http'; // Para ter acesso a requisições de serviço
 
 @Component({
     moduleId: module.id,
@@ -15,5 +19,13 @@ import { Component } from '@angular/core';
 // é preciso o export pra dizer que é um modulo exportavel no ES6
 export class AppComponent {
 
+    // O Angular vai buscar o http pegando quem ele depende. Para isso precisa usar @Inject (um decorator a ser importado)
+    // Pegue uma instancia do Http e passe como parametro para mim no construtor
+    // constructor(@Inject(Http) http) { // Se fosse sem usar o recurso da tipagem do TypeScript
+    constructor(http: Http) {
+        
+        // Criar uma requisição Http é preciso passar 2 parametros que o Angular faz isso via Injeção de Dependência
+        // let http = new Http(); // -- Assim não funcionaria
+    }
 
 }
--- alurapic/client/app/app.module.ts
+++ alurapic/client/app/app.module.ts
@@ -6,12 +6,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; // obrigatorio
 import { AppComponent } from './app.component'; // dará erro se nao utilizar export antes da class AppComponent
 import { FotoModule } from './foto/foto.module';
+import { HttpModule } from '@angular/http';
 
 // Transforma sua classe num module do Angular
 @NgModule({
     // Passa um array com todos os modules que eu quero importar
     // Como a aplicação vai rodar no Browser, preciso importar o BrowserModule
-    imports: [ BrowserModule, FotoModule ], // Porque AppModule depende de FotoModule (por isso importou ele aqui)
+    imports: [ BrowserModule, FotoModule, HttpModule ], // Porque AppModule depende de FotoModule (por isso importou ele aqui)
     
     // Declarar quais ou outros componentes ou recursos que ele tem
     declarations: [ AppComponent ],
