06d9045ddc47273b9ad03a8c65b3a5df063a1a45
Jordana Morais <jordana.morais@liferay.com>
2017-08-29 17:43:45 -0300

    Created register method

--- alurapic/client/app/cadastro/cadastro.component.ts
+++ alurapic/client/app/cadastro/cadastro.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core'
+import { Component } from '@angular/core';
+import { FotoComponent } from '../foto/foto.component';
+import { Http, Headers } from '@angular/http'; // Para ter acesso a requisições de serviço e o Headers para o ultimo param do post
 
 @Component({
     moduleId: module.id,
@@ -10,4 +12,40 @@ import { Component, Input } from '@angular/core'
 // ( e nao sera reutilizado em outros lugares como as fotos, vou chamar apenas no module principal)
 export class CadastroComponent {
 
+    // Faz com que eu enxergue os atributos disponíveis de FotoComponent.
+    foto: FotoComponent = new FotoComponent();
+    http: Http; // guardar para poder utilizar o http no método cadastrar
+
+    constructor(http: Http) {
+
+        // testando inicialização
+        //this.foto.titulo = 'A';
+        //this.foto.url = 'B';
+        //this.foto.descricao = 'C';
+
+        this.http = http; // definido acima e que será reusado.
+    }
+    // Os dados serao enviados via ajax com o Angular
+    cadastrar(event) {
+
+        event.preventDefault();
+
+        // Obrigatorio para o ultimo parametro passado ao usar o post();
+        let postHeaders = new Headers();
+        postHeaders.append('Content-Type', 'application/json');
+
+        // No padrão REST, se eu mando post() é porque quero incluir um novo recurso.
+        this.http
+            .post('v1/fotos', JSON.stringify(this.foto), { headers: postHeaders})
+            .subscribe(() => { // executa um codigo quando a requisicao é bem sucedida
+
+                // Dentro do subscribe se limpa o form, por exemplo.
+
+                // vai refletir o dado mais atualizado na view. Também uma maneira de limpar o form.
+                this.foto = new FotoComponent(); 
+                console.log('Foto Salva com Sucesso');
+            }, erro => {
+                console.log('Erro ao tentar salvar a foto.');
+            });
+    }
 }
