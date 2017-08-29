65598f83239ed75827d1f6648a22b64e78bfa141
Jordana Morais <jordana.morais@liferay.com>
2017-08-29 17:44:23 -0300

    Truncating titulo text

--- alurapic/client/app/painel/painel.component.ts
+++ alurapic/client/app/painel/painel.component.ts
@@ -10,4 +10,10 @@ export class PainelComponent {
 
     // permite esse parametro estar disponivel no componente
     @Input() titulo: string; // a tipagem é opcional. Tipa se quiser.
+
+    constructor() {
+        this.titulo = this.titulo.length > 7  
+            ? this.titulo.substr(0,7) + "..."
+            : this.titulo;
+    }
 }
